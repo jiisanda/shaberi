ec0554f627107686faea7b89f186bab014bf8db5
heyharshjaiswal <harshjaiswal2307@gmail.com>
2022-02-11 00:35:11 +0530

    userauth-forms

--- userauth/urls.py
+++ userauth/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from userauth import views
 
+app_name = 'userauth'
+
 urlpatterns = [
     path('login/', views.user_login_view, name="user_login"),
     path('logout/', views.user_logout, name="logout"),
--- userauth/views.py
+++ userauth/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import User
+
 from django.http import HttpResponseRedirect
 
 from django.contrib import messages
@@ -10,7 +12,7 @@ from django.urls import reverse
 
 from django.views.generic import DetailView
 
-from userauth.forms import UserForm, ProfileForm
+from userauth.forms import UserForm
 from userauth.models import UserProfileInfo
 
 # Create your views here.
@@ -39,26 +41,71 @@ def user_login_view(request):
 
 
 def registrationView(request):
-    registered = False
+    # if this is the POST request we need to process the form data
+    template = 'userauth/registration.html'
     if request.method == "POST":
-        user_form = UserForm(data=request.POST)
-        profile_form = ProfileForm(data=request.POST)
-        if user_form.is_valid() and profile_form.is_valid():
-            user = user_form.save()
-            user.set_password(user.password)
-            user.save()
+        # creating a form instance and populate it with data from the request: 
+        form = UserForm(request.POST)
+        # checking the form is valid
+        if form.is_valid():
+            if User.objects.filter(username=form.cleaned_data['username']).exists():
+                return render(request, template, {
+                    'form':form,
+                    'error_message':'Username already exists.'
+                })
+            elif User.objects.filter(email=form.cleaned_data['email']).exists():
+                return render(request, template, {
+                    'form':form,
+                    'error_message':'Email already exists.'
+                })
+            elif form.cleaned_data['password'] != form.cleaned_data['password_repeat']:
+                return render(request, template, {
+                    'form':form,
+                    'error_message':'Passwords do not match.'
+                })
+            else:
+                # Create the user:
+                user = User.objects.create_user(
+                    form.cleaned_data['username'],
+                    form.cleaned_data['email'],
+                    form.cleaned_data['password']
+                )
+                user.first_name = form.cleaned_data['first_name']
+                user.last_name = form.cleaned_data['last_name']
+                user.phone_number = form.cleaned_data['phone_number']
+                user.save()
 
-            profile = profile_form.save(commit=False)
-            if 'profile_picture' in request.FILES:
-                profile.profile_picture = request.FILES['profile_picture']
-            profile.save()
-            registered = True
-        else:
-            print(user_form.errors, profile_form.errors)
+                # Login the user
+                login(request, user)
+
+                # redirect to home page:
+                return HttpResponseRedirect(reverse('home'))
+    
+    # No post data available, showing the same page
     else:
-        user_form = UserForm()
-        profile_form = ProfileForm()
-    return render(request, 'userauth/registration.html', {'user_form':user_form,'profile_form':profile_form, 'registered':registered})
+        form = UserForm()
+    
+    return render(request, template, {'form':form})
+
+
+
+    #     profile_form = ProfileForm(data=request.POST)
+    #     if user_form.is_valid() and profile_form.is_valid():
+    #         user = user_form.save()
+    #         user.set_password(user.password)
+    #         user.save()
+
+    #         profile = profile_form.save(commit=False)
+    #         if 'profile_picture' in request.FILES:
+    #             profile.profile_picture = request.FILES['profile_picture']
+    #         profile.save()
+    #         registered = True
+    #     else:
+    #         print(user_form.errors, profile_form.errors)
+    # else:
+    #     user_form = UserForm()
+    #     profile_form = ProfileForm()
+    # return render(request, 'userauth/registration.html', {'user_form':user_form,'profile_form':profile_form, 'registered':registered})
 
 
 # class ShowProfileView(DetailView):
