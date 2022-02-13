241015505420d647868501409850c9e362f37487
heyharshjaiswal <harshjaiswal2307@gmail.com>
2022-02-13 23:58:35 +0530

    load-message-initial

--- chats_app/models.py
+++ chats_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 # Create your models here.
+class Message(models.Model):
+    author = models.ForeignKey(User, related_name='author_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author.username
+    
+    def last_20_messages(self):
+        return Message.objects.order_by('-timestap').all()[:20]
