25c73ce58e7bb9cf6153ca9d5389bf443c3bbb77
heyharshjaiswal <harshjaiswal2307@gmail.com>
2022-02-14 12:46:50 +0530

    change in base.html

--- chats_app/urls.py
+++ chats_app/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from chats_app.views import HomeView
 from . import views
 
+app_name = 'chats_app'
+
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('chat/', views.index, name="index"),
